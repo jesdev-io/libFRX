63014b8900d1a0af5ce1389f5a3ba68ef61fa42e
jake-is-ESD-protected <j.tschavoll@tu-berlin.de>
2026-06-27 12:23:04 +0200

    add: baseline system defs

---
+++ include/libfrx_sys.h
@@ -0,0 +1,9 @@
+#ifndef _LIBFRX_SYS_
+#define _LIBFRX_SYS_
+
+#define LIBFRX_SYS_PFX          "libFRX"
+#define LIBFRX_SYS_INFO_PFX     LIBFRX_SYS_PFX " INFO: "
+#define LIBFRX_SYS_WARN_PFX     LIBFRX_SYS_PFX " WARN: "
+#define LIBFRX_SYS_FAIL_PFX     LIBFRX_SYS_PFX " FAIL: "
+
+#endif // _LIBFRX_SYS_
