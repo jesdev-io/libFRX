0caa77344688cfede83ee97ca2646566505b8798
jake-is-ESD-protected <j.tschavoll@tu-berlin.de>
2026-06-28 16:04:22 +0200

    fix: move debug print to sys

--- include/libfrx_sys.h
+++ include/libfrx_sys.h
@@ -6,4 +6,12 @@
 #define LIBFRX_SYS_WARN_PFX     LIBFRX_SYS_PFX " WARN: "
 #define LIBFRX_SYS_FAIL_PFX     LIBFRX_SYS_PFX " FAIL: "
 
+#ifdef LIBFRX_SYS_DEBUG_PRINT_ENABLE
+#define LIBFRX_SYS_DEBUG_PRINT(format, ...) jes_print(format, ##__VA_ARGS__)
+#define LIBFRX_SYS_DEBUG_PRINT_PJ(pj, format, ...) jes_print_pj(pj, format, ##__VA_ARGS__)
+#else
+#define LIBFRX_SYS_DEBUG_PRINT(format, ...)
+#define LIBFRX_SYS_DEBUG_PRINT_PJ(pj, format, ...)
+#endif
+
 #endif // _LIBFRX_SYS_
